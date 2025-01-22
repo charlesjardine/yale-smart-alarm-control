e12e9f32fdac7d45e62826f998adb965b576358c
Chas Jardine <charles.jardine@nathan-software.com>
2025-01-22 11:17:28 +0000

    Update YaleAlarm.cs

--- YaleAlarm.cs
+++ YaleAlarm.cs
@@ -23,7 +23,7 @@ namespace YaleAlarm
         const string _ENDPOINT_SET_MODE = "/api/panel/mode/";
         const string _ENDPOINT_DEVICES_STATUS = "/api/panel/device_status/";
 
-        const string _YALE_AUTH_TOKEN = "VnVWWDZYVjlXSUNzVHJhcUVpdVNCUHBwZ3ZPakxUeXNsRU1LUHBjdTpkd3RPbE15WEtENUJ5ZW1GWHV0am55eGhrc0U3V0ZFY2p0dFcyOXRaSWNuWHlSWHFsWVBEZ1BSZE1xczF4R3VwVTlxa1o4UE5ubGlQanY5Z2hBZFFtMHpsM0h4V3dlS0ZBcGZzakpMcW1GMm1HR1lXRlpad01MRkw3MGR0bmNndQ==";
+        const string _YALE_AUTH_TOKEN = "xxxxx";
         WebClient client;
         ResonseModel resonseModel;
 
